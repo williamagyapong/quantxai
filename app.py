7c2bf0b27eab2abe2d3ccc9d5ac5a93139df0fb4
William Ofosu Agyapong <willisco413@gmail.com>
2024-08-11 21:46:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.write("Hello world!") 
+st.title("Data Analysis with PandasAI")
+
+uploaded_file = st.sidebar.file_uploader(
+    "Upload a CSV file",
+    type=['csv']
+    )
+
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write(data.head())
+    st.write(data.shape)
